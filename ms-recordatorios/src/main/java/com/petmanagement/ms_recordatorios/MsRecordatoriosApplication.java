87c150faee153be2258a00d2a41d1a664720b747
VI-Alumno <VI-Alumno@duoc.acad>
2026-04-17 13:20:44 -0400

    folders creation

---
+++ ms-recordatorios/src/main/java/com/petmanagement/ms_recordatorios/MsRecordatoriosApplication.java
@@ -0,0 +1,13 @@
+package com.petmanagement.ms_recordatorios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRecordatoriosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRecordatoriosApplication.class, args);
+	}
+
+}
---
+++ ms-recordatorios/src/test/java/com/petmanagement/ms_recordatorios/MsRecordatoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petmanagement.ms_recordatorios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRecordatoriosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
