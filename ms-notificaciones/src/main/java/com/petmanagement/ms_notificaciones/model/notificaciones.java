7816ab5aaea64ec81e2e23d798c6b02476199dc7
Miumochii <diegoboggle@icloud.com>
2026-04-20 09:54:50 -0400

    model complete

--- ms-notificaciones/src/main/java/com/petmanagement/ms_notificaciones/model/notificaciones.java
+++ ms-notificaciones/src/main/java/com/petmanagement/ms_notificaciones/model/notificaciones.java
@@ -1,9 +1,23 @@
+package com.petmanagement.ms_notificaciones.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Notificaciones {
+public class notificaciones {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
