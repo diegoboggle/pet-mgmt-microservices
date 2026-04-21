0eba6d3cb6fbc01ebe9de55c8f63771a9cc98446
javiv2 <ja.cofrel@duocuc.cl>
2026-04-21 11:12:28 -0400

    model update

--- ms-vacunas/src/main/java/com/example/msvacunas/model/Vacunas.java
+++ ms-vacunas/src/main/java/com/example/msvacunas/model/Vacunas.java
@@ -1,5 +1,47 @@
 package com.example.msvacunas.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "vacunas")
+@Data
+@NoArgsConstructor
+@allArgsConstructor
 public class Vacunas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long mascotaId;
+    
+    @Column(nullable = false)
+    private String nombreVacuna;
+
+    @Column(nullable = false)
+    private String marca;
+
+    @Column(nullable = false)
+    private int numeroDosis;
+
+    @Column(nullable = false)
+    private LocalDate fechaAplicacion;
+
+    @Column(nullable = true)
+    private LocalDate fechaProximoRefuerzo;
+
+    @Column(nullable = false)
+    private String veterinarioAplicador;
+
+    @Column(nullable = true)
+    private String observaciones;
 
 }
